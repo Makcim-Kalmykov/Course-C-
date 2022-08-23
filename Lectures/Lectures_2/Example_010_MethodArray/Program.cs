e6b5c4b5934dde4f7450c3c87669ecc9fc91085a
Maksim Kalmykov <makc_mail@list.ru>
2022-08-23 16:36:55 +0300

    Add lectures 2

---
+++ Lectures/Lectures_2/Example_010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {1, 2, 3, 4, 5, 6, 4, 7, 8, 9};
+
+int n = array.Length; //присваиваем переменной "n" значение, которое равно количеству элементов массива
+
+int find = 4;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)  //если индекс массива совпадает (равен) значению переменной find
+    {
+        Console.WriteLine(index);
+        break; //прервать условие, больше не выполнять цикл
+    }
+    index++; //index = index + 1;
+}
---
+++ Lectures/Lectures_2/Example_011_ArrayLibrary/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;  //получаем длинну массива
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+        while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;   //после того как нашли первое совпадение прерываем цикл
+        }
+        index++;
+
+    }
+    return position;
+}
+
+int[] array = new int[10]; //создай новый массив, в котором будет 10 элементов
+
+
+FillArray(array); // вызвали метод FillArray, который заполнил массив
+PrintArray(array); // вызвали метод PrintArray, который вывел в консоль массив
+Console.WriteLine();
+
+int pos = IndexOff(array, 5); //в качестве аргуента массив, ищем 4
+Console.WriteLine(pos);

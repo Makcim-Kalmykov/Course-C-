0fe2b98604ef4f0709b4e847f7b158312f8ec5d3
Maksim Kalmykov <makc_mail@list.ru>
2022-09-06 01:03:31 +0300

    Add Seminar 4 + HomeWork

---
+++ Seminar/Seminar_4/HomeWork/Program.cs
@@ -0,0 +1,74 @@
+﻿//**********************************************************************************************************************
+//***Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B***
+//**********************************************************************************************************************
+
+/*
+int Exponentiation (int number1, int number2)
+{
+    int count = number1;
+    for (int i = 1; i < number2; i++) count *= number1;
+    return count;
+}
+
+Console.Write("Enter number A: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter number B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Number {num1} to the power of {num2} is equal to {Exponentiation(num1, num2)}");
+
+*/
+
+//************************************************************************************************
+//***Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе***
+//************************************************************************************************
+
+/*
+
+int Sum(string number)
+{
+    int [] arr = new int [number.Length];
+    for(int i = 0; i < number.Length; i++) arr[i] = Convert.ToInt32(number.Substring(i, 1)); //Записываем число в массив
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++) count += arr[i];
+    return count;
+}
+
+Console.Write("Enter integer numbers: ");
+string sumNumbers = Console.ReadLine();
+Console.WriteLine($"The sum of the digist of the number {sumNumbers} = {Sum(sumNumbers)}");
+
+*/
+
+//***********************************************************************************************
+//***Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран***
+//***********************************************************************************************
+
+int [] Arr (int number) //метод записи массива пользователем через консоль
+{
+    int [] array = new int [number];
+    for(int i = 0; i < number; i++)
+    {
+        Console.Write($"Enter number №{i+1} of the array: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+
+void PrintArray (int [] array) //метод печати массива
+{
+    Console.Write("[");
+    for(int i = 0; i < array.Length; i++) Console.Write($"{array[i]}, ");
+    Console.Write("\b\b]");
+}
+
+Console.Write("Enter the required number of array numbers: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+
+PrintArray(Arr(quantity));
+
+
+
+
---
+++ Seminar/Seminar_4/Seminar/Program.cs
@@ -0,0 +1,117 @@
+﻿//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%% Задача 1. Принимает число N и выдает сумму чисел от 1 до N %%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+/*
+
+int FindSum(int a)
+{
+    int sum = 0;
+    for(int current = 1; current<=a; current++) sum += current;
+    return sum;
+}
+
+Console.WriteLine("Input positive integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"sum of numbers from 1 to {num} is {FindSum(num)}");
+
+*/
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%% Задача 2. Принимает на вход число и возвращает кол-в оцифр в этом числе (через строку, через деление на 10) %%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+//------------%%% Вариант 1 %%%-------------
+/*
+int DigitsNumber(int number)
+{
+    string num = number.ToString(); //переводим число в строку
+    int result = num.Length; 
+    return result;
+}
+
+Console.WriteLine("Inter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Number of digits of the number {number} = {DigitsNumber(number)}");
+
+*/
+
+//------------%%% Вариант 2 %%%%-------------
+/*
+int DigitsNumber(int number)
+{
+    int result = 1;
+    for(int current = 1; number > 10; current++)
+    {
+        number /= 10; 
+        result ++;
+    } 
+    return result;
+}
+
+Console.WriteLine("Inter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Number of digits of the number {number} = {DigitsNumber(number)}");
+
+*/
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+//%%% Задача 3. Найти факториал %%%
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+/*
+
+int Factorial (int number)
+{
+    int result = 1;
+    for(int temp = 1; temp <= number; temp++) result = result * temp;
+    return result;
+}
+
+Console.WriteLine("Inter positive integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Factorial number {number} = {Factorial(number)}");
+
+*/
+
+
+//***Выводит массив из 8 элементов, заполненных 0 и 1 в случайном порядке (метод Random Next)***
+
+/* мой вариант
+void Random (int [] array)
+{
+    int [] arr = new int [8];
+    Random rand = new Random();
+    for(int i = 0; i < array.Length; i++) arr[i] = rand.Next(0, 1);
+    return arr;
+}
+
+int[] array = Random;
+
+Console.WriteLine(arrays);
+
+*/
+
+// из зала
+
+int [] CreateRandomArray(int n, int minVal, int maxVal)
+{
+    int [] arr = new int [n];
+    for(int i = 0; i < n; i++) arr[i] = new Random().Next(minVal,maxVal+1); // новый обьект класса Random, который вызывает метод Next, который возвращает цифру
+    return arr;
+}
+
+
+void PrintArray(int [] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i<arr.Length; i++) Console.Write($"{arr[i]}, ");
+    Console.WriteLine("\b\b ]");
+}
+
+PrintArray(CreateRandomArray(8, 0, 1));
+
+//int [] newArr = CreateRandomArray(8, 0, 1); создали сразу новый массив с результатом работы метода
+//PrintArray(newArr);
